16be3f575f636a7a59a619dc5657c226c6a4947b
adilet berdibekov <berdibekovadilet@gmail.com>
2022-10-08 18:16:06 +0600

    added getAll devices

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -24,7 +24,36 @@ class DeviceController {
     }
   }
   async getAll(req, res) {
-    const devices = await Device.findAll();
+    let { brandId, typeId, limit, page } = req.query;
+    page = page || 1;
+    limit = limit || 9;
+    let offset = page * limit - limit;
+    let devices;
+    if (!brandId && !typeId) {
+      devices = await Device.findAndCountAll({ limit, offset });
+    }
+    if (brandId && !typeId) {
+      devices = await Device.findAndCountAll({
+        where: { brandId },
+        limit,
+        offset,
+      });
+    }
+    if (!brandId && typeId) {
+      devices = await Device.findAndCountAll({
+        where: { typeId },
+        limit,
+        offset,
+      });
+    }
+    if (brandId && typeId) {
+      devices = await Device.findAndCountAll({
+        where: { brandId, typeId },
+        limit,
+        offset,
+      });
+    }
+
     return res.json(devices);
   }
   async getOne(req, res) {}
