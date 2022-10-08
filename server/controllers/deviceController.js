b280c447d70536a7318d621f11a6115fa09b52b5
adilet berdibekov <berdibekovadilet@gmail.com>
2022-10-08 18:02:01 +0600

    added create device

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,6 +1,32 @@
+const uuid = require("uuid");
+const path = require("path");
+const { Device } = require("../models/models");
+const ApiError = require("../error/ApiError");
+
 class DeviceController {
-  async create(req, res) {}
-  async getAll(req, res) {}
+  async create(req, res, next) {
+    try {
+      const { name, price, typeId, brandId, info } = req.body;
+      const { img } = req.files;
+      let filename = uuid.v4() + ".jpg";
+      img.mv(path.resolve(__dirname, "..", "static", filename));
+
+      const device = await Device.create({
+        name,
+        price,
+        typeId,
+        brandId,
+        img: filename,
+      });
+      return res.json(device);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
+  }
+  async getAll(req, res) {
+    const devices = await Device.findAll();
+    return res.json(devices);
+  }
   async getOne(req, res) {}
 }
 
