162abd04e6ec119b89a1fd69b1f8600657bf96f7
adilet berdibekov <berdibekovadilet@gmail.com>
2022-10-09 10:41:42 +0600

    added registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,28 @@
 const ApiError = require("../error/ApiError");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User, Basket } = require("../models/models");
 
 class UserController {
-  async registration(req, res) {}
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
+    if (!email || !password) {
+      return next(ApiError.badRequest("Некорректый email или пароль"));
+    }
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return next(
+        ApiError.badRequest("Пользователь с таким email уже существует")
+      );
+    }
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, role, password: hashPassword });
+    const basket = await Basket.create({ userId: user.id });
+    const jwt = jwt.sign({ id: user.id, email, role }, process.env.SECRET_KEY, {
+      expiresIn: "24h",
+    });
+    return res.json({ token });
+  }
   async login(req, res) {}
   async check(req, res, next) {
     const { id } = req.query;
