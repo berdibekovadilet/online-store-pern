720238e4bbb0185aeba96472f2660832a1377b87
adilet berdibekov <berdibekovadilet@gmail.com>
2022-10-09 11:10:07 +0600

    added login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -27,7 +27,19 @@ class UserController {
     const token = generateJwt(user.id, user.email, user.role);
     return res.json({ token });
   }
-  async login(req, res) {}
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return next(ApiError.badRequest("Пользователь не найден"));
+    }
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.badRequest("Неверный пароль"));
+    }
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
+  }
   async check(req, res, next) {
     const { id } = req.query;
     if (!id) {
