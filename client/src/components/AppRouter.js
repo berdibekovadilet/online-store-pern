e1c163a075176ad6904cd2fe79708460f48c9acd
adilet berdibekov <berdibekovadilet@gmail.com>
2022-10-11 12:07:57 +0600

    added UserStore via MobX

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import { authRoutes, publicRoutes } from "../routes";
+import NotFound from "../pages/NotFound";
+import { Context } from "..";
 
 const AppRouter = () => {
-  const isAuth = false;
+  const { user } = useContext(Context);
+  console.log("====================================");
+  console.log(user);
+  console.log("====================================");
   return (
     <Routes>
-      {isAuth &&
+      {user.isAuth &&
         authRoutes.map(({ path, Component }) => (
           <Route key={path} path={path} element={<Component />} exact />
         ))}
       {publicRoutes.map(({ path, Component }) => (
         <Route key={path} path={path} element={<Component />} exact />
       ))}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
